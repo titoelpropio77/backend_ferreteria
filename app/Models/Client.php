2ad8d56a0b823e36eddd3b43ae4fde46a5887f30
titoelpropio77 <titoelpropio77@gmail.com>
2025-03-09 19:43:18 -0400

    Integracion Maestro Client

---
+++ app/Models/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'birth_date'];
+}

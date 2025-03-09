5653e10e77a737934c2fbae62a815e0d9d2def63
titoelpropio77 <titoelpropio77@gmail.com>
2025-03-09 19:53:45 -0400

    Integracion maestro producto

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('clients', ClientController::class);
 Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('products', ProductController::class);

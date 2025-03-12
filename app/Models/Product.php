e155d52d7adb71c022c05c3c3dcb2496785945ef
Modesto <modesto@goodmeal.app>
2025-03-11 22:51:46 -0400

    Integracion Test de producto

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'price', 'stock', 'category', 'brand', 'active'];
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'price', 'stock', 'category', 'brand'];
 }
